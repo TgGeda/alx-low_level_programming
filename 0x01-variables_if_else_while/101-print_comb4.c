6c185e60ad9cf0203bd4af44f9f69746ab224b3f
Birha <bura.abuy2015@gmail.com>
2022-12-09 20:16:35 -0800

    u

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,12 +25,12 @@ int main(void)
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-			          		continue;
+			           continue;
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-putchar('\n');
-return (0);
+ putchar('\n');
+ return (0);
 }
