5cfc7921757cce8a20c7090ca0764c9b24c1790d
Birhan Abuhay Jemere <60174986+Birha@users.noreply.github.com>
2023-02-01 21:22:13 +0300

    Update 0-malloc_checked.c
    Littele Correction

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,17 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocate memory
- * @b: number of bytes to allocate
- * Return: pointer to allocated memory
+ * malloc_checked - Main Entry
+ * @b: input
+ * Return: 0
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+	void *p;
 
+	p = malloc(b);
 	if (p == NULL)
 		exit(98);
 	return (p);
-	
 }
